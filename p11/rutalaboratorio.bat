899ac13830fd6b8337b6ba3749950e79f9468405
David Rico de los Bueis <UO306810@uniovi.es>
2026-02-04 19:53:02 +0100

    fin de clase p11

--- p11/rutalaboratorio.bat
+++ p11/rutalaboratorio.bat
@@ -1,2 +1,2 @@
-set path=C:\Program Files\Java\<jdk>\bin
-set classpath=C:\Users\<loquesea>\<repositorio github>\p11
+set path=C:\Program Files\Java\jdk-24\bin
+set classpath=C:\Users\uo306810\Desktop\alg_DavidRicoDeLosBueisUO306810\p11
