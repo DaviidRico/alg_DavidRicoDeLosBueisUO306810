93642adde807a27175bca2951f44331506156376
DaviidRico <UO306810@uniovi.es>
2026-03-11 19:59:18 +0100

    clase 11/03/26

---
+++ p4/p4JavaProject/src/ColoreoGrafo.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ColoreoGrafo {
+    
+    private List<String> colors = Arrays.asList(
+    "red", "blue", "green",
+    "yellow", "orange", "purple",
+    "cyan", "magenta", "lime");
+
+    private final static int  MAX_CONEX = 8;
+
+    static public Map<String, String> realizarVoraz(Map<String, List<String>> grafo) {
+        Map<String, String> result = new Map<String, String>();
+
+        for(String elemnto: grafo.keySet()){
+
+        }
+        
+    }
+
+    //recorro jason luego miro los vecinos dentro de result
+    //map contains key(3)
+    //metodo para recorrer el jason con un for
+}
